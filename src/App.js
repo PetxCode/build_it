6646e279f60e4f590091f449b8417212561ff320
PetxCode <brighterdayscodelab@gmail.com>
2020-09-29 22:11:29 +0100

    starting commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    const url = "https://jsonplaceholder.typicode.com/users";
+    fetch(url).then((response) =>
+      response.json().then((response) => {
+        console.log(response);
+        this.setState({
+          users: response,
+        });
+      })
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Tech Talk Conference</h1>
+          <a
+            className="App-link"
+            href="https://brighterdayscodelab.com.ng"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by BDCL students
+          </a>
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <div>
+            {this.state.users.map((user) => (
+              <h3>{user.name}</h3>
+            ))}
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
